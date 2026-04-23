8bffffba81887d6b10a80985933486db50102af0
devvk <info@sekretov.net>
2026-04-23 17:40:51 +0200

    Spring Data JPA [#505298]

--- src/main/java/ru/job4j/bmb/model/User.java
+++ src/main/java/ru/job4j/bmb/model/User.java
@@ -1,9 +1,21 @@
 package ru.job4j.bmb.model;
 
+import jakarta.persistence.*;
+
+import java.util.Objects;
+
+@Entity
+@Table(name = "mb_user")
 public class User {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @Column(name = "client_id")
     private long clientId;
+
+    @Column(name = "chat_id")
     private long chatId;
 
     public long getClientId() {
@@ -20,5 +32,18 @@ public class User {
 
     public long getChatId() {
         return chatId;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        User user = (User) o;
+        return Objects.equals(id, user.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
     }
 }
--- src/main/java/ru/job4j/bmb/repository/UserRepository.java
+++ src/main/java/ru/job4j/bmb/repository/UserRepository.java
@@ -1,15 +1,12 @@
 package ru.job4j.bmb.repository;
 
+import org.springframework.data.repository.CrudRepository;
 import ru.job4j.bmb.model.User;
 
 import java.util.List;
 
-public interface UserRepository {
+public interface UserRepository extends CrudRepository<User, Long> {
 
     List<User> findAll();
-
-    User findByClientId(Long clientId);
-
-    void add(User user);
 }
 
