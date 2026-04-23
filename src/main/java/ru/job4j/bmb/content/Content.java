3e2351a4d8a92b9eca1dce25a37e6cef1d354806
devvk <info@sekretov.net>
2026-04-23 15:01:23 +0200

    Внедрение коллекций | Поставщики контекста [#505300]

--- src/main/java/ru/job4j/bmb/content/Content.java
+++ src/main/java/ru/job4j/bmb/content/Content.java
@@ -1,4 +1,29 @@
 package ru.job4j.bmb.content;
 
+import org.telegram.telegrambots.meta.api.objects.InputFile;
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.InlineKeyboardMarkup;
+
 public class Content {
+
+    private final Long chatId;
+    private String text;
+    private InputFile photo;
+    private InlineKeyboardMarkup markup;
+    private InputFile audio;
+
+    public Content(Long chatId) {
+        this.chatId = chatId;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+
+    public void setPhoto(InputFile inputFile) {
+        this.photo = inputFile;
+    }
+
+    public void setAudio(InputFile inputFile) {
+        this.audio = inputFile;
+    }
 }
